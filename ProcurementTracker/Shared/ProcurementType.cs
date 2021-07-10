28ffc63198e264100797826af97bad00a2797eb5
Joseph Serunjogi <jserunjogi@touchpointmed.com>
2021-07-10 18:29:08 +0300

    Closes #29 - Setup procurement types

---
+++ ProcurementTracker/Shared/ProcurementType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcurementTracker.Shared
+{
+    public class ProcurementType
+    {
+        public ProcurementType(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; private set; }
+
+        public static ProcurementType SUPPLIES { get { return new ProcurementType("SUPPLIES"); } }
+
+        public static ProcurementType WORKS { get { return new ProcurementType("WORKS"); } }
+
+        public static ProcurementType SERVICES { get { return new ProcurementType("SERVICES"); } }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
