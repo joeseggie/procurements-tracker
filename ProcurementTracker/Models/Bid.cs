1d450f8e978087164c93b3eef2e3cfa0ca9788f1
Joseph Serunjogi <jserunjogi@touchpointmed.com>
2021-09-08 15:06:04 +0300

    Closes #97 - resolve unhandled exception

--- ProcurementTracker/Models/Bid.cs
+++ ProcurementTracker/Models/Bid.cs
@@ -10,24 +10,24 @@ namespace ProcurementTracker.Models
     public class Bid
     {
         public Guid Id { get; set; }
-        public string Status { get; set; }
+        public string? Status { get; set; }
 
         [DataType(DataType.Date)]
-        public DateTime Submitted { get; set; }
+        public DateTime? Submitted { get; set; }
 
         [Display(Name = "Valid Period (Days)")]
-        public int ValidPeriod { get; set; }
-        public Supplier Supplier { get; set; }
-        public Procurement Procurement { get; set; }
+        public int? ValidPeriod { get; set; }
+        public Supplier? Supplier { get; set; }
+        public Procurement? Procurement { get; set; }
 
         [Column(TypeName = "decimal(18,2)")]
         [Display(Name = "Quoted Fee")]
         [DisplayFormat(DataFormatString = "{0:N}", ApplyFormatInEditMode = false)]
-        public decimal QuotedFee { get; set; }
+        public decimal? QuotedFee { get; set; }
 
-        public string RejectionReason { get; set; }
+        public string? RejectionReason { get; set; }
 
-        public string StatusColorCode
+        public string? StatusColorCode
         {
             get
             {
