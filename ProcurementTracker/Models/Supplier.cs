d7151a795f2ed66c28fc435490486d9afd2cd956
Joseph Serunjogi <jserunjogi@touchpointmed.com>
2021-09-09 12:54:32 +0300

    Closes #96 - Resolve unhandled exception

--- ProcurementTracker/Models/Supplier.cs
+++ ProcurementTracker/Models/Supplier.cs
@@ -9,12 +9,12 @@ namespace ProcurementTracker.Models
     public class Supplier
     {
         public Guid Id { get; set; }
-        public string Contact { get; set; }
+        public string? Contact { get; set; }
 
         [Display(Name = "Service Provider")]
-        public string Name { get; set; }
-        public string Address { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
 
-        public List<Bid> Bids { get; set; }
+        public List<Bid>? Bids { get; set; }
     }
 }
